e07b9473ad47b476764917f1ef7d0238d014357c
Guinho Anjos <iago.anjos@hotmail.com>
2021-03-25 17:55:51 -0300

    exercicios dia 2

---
+++ bloco_4/js-exercises2.2.3.js
@@ -0,0 +1,8 @@
+//Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let xablau of names){
+    xablau= ('joao ' + xablau)
+    console.log(xablau);
+}
