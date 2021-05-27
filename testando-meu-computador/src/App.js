9d91a6bef8463978526b58915e034bb1d59be2b2
guinhoanjos <iago.anjos@hotmail.com>
2021-05-27 17:30:12 -0300

    Update App.js

--- testando-meu-computador/src/App.js
+++ testando-meu-computador/src/App.js
@@ -1,39 +1,44 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-const Task = (value) => {
-  return (
-    <li key={value}>{value}</li>
-  );
-}
 
-const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+class App extends React.Component {
+  constructor() {
+    super()
+    this.clicker = this.clicker.bind(this)
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <ol>
-      {tarefas.map((value) => {
-        return Task(value);
-      })}
-      </ol>
-      
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  clicker() {
+    console.log(this.context)
+  }
+
+  render(){
+    
+
+    return (
+      <div className="App">
+        <button onClick={this.clicker}>Click 1</button>
+        <button >Click 2</button>
+        <button >Click3</button>
+        
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
